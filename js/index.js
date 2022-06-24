bc4d93232c5d36e4e157d0fc0968b401a18f13c2
YurijCroz <valetkrest@gmail.com>
2022-06-24 11:20:00 +0300

    spread across functions

--- js/index.js
+++ js/index.js
@@ -3,15 +3,22 @@ let container = document.querySelector('div.container');
 
 const setImg = document.querySelectorAll('div.min-size');
 
-function depFun(){
-    let fullImg = document.querySelector('div.full-size'); //берем предыдущий элемент полного размера
-    fullImg.classList.remove('full-size');  //удаляем у него тот самый класс который дает ему полный размер
+function addFullSizeClass(){
+    removeFullSizeClass()
     this.classList.add('full-size');  //присваеваем класс полного размера элементу на который нажали
-    let imgBG = document.querySelector('div.full-size img').getAttribute('src'); //берем адресс картинки элемента полного размера
-    container.style.backgroundImage = `url(${imgBG})`; //присваеваем полученный адресс на бэк контэйнера
+    backgroundImageReplacement()
 }
 
-setImg.forEach(el=> el.addEventListener('click', depFun));
+function removeFullSizeClass() {
+    let fullImg = document.querySelector('div.full-size'); //берем предыдущий элемент полного размера
+    fullImg.classList.remove('full-size');  //удаляем у него тот самый класс который дает ему полный размер
+}
 
-let imgBG = document.querySelector('div.full-size img').getAttribute('src');  //тоже самое что и в функции, пришлось и тут сделать потому что в css идут какие то траблы
-container.style.backgroundImage = `url(${imgBG})`;
+function backgroundImageReplacement(){
+    let imgBG = document.querySelector('div.full-size img').getAttribute('src'); //берем адресс картинки элемента full-size
+    container.style.backgroundImage = `url(${imgBG})`; //присваеваем полученный адресс на бэк контэйнера
+};
+
+setImg.forEach(el=> el.addEventListener('click', addFullSizeClass));
+
+backgroundImageReplacement()
